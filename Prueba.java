74cfa9f59d57593ce3e39cdb5eb30d7f28a73809
cesar <cesarks81@gmail.com>
2024-09-24 09:09:34 +0200

    Actualiza Prueba.java para incluir mi comida favorita.

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
     public static void main(String[] args){
         System.out.println("Ramos Morón");
         System.out.println("19 años, año: 2004");
+        System.out.println("Los espagueti a la boloñesa");
     }
 }
